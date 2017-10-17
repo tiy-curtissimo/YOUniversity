7622edbdd14b0fddf649381021fed6f355d946ed
Peter Boudreau <Peter.Boudreau@LibertyMutual.com>
2017-10-17 16:16:19 -0400

    trying to change app.main for CORS issue

--- src/main/java/com/libertymutual/goforcode/youniversity/YOUniversityApplication.java
+++ src/main/java/com/libertymutual/goforcode/youniversity/YOUniversityApplication.java
@@ -19,7 +19,7 @@ public class YOUniversityApplication {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/*").allowedOrigins("*");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
